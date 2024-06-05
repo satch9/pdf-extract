5ab82ef397e2154ab414b3f3ae74f5bf6b67cac7
satch9 <viny1976@gmail.com>
2024-06-05 15:30:25 +0400

    l'api est en place. Projet réalisé en html est fini

--- index.js
+++ index.js
@@ -1,149 +1,202 @@
-import express from 'express'
-import fileUpload from 'express-fileupload'
-import path from 'path'
-import fs from 'fs'
-import { PDFExtract } from 'pdf.js-extract'
+import express from "express";
+import fileUpload from "express-fileupload";
+import path from "path";
+import fs from "fs";
+import { PDFExtract } from "pdf.js-extract";
 
-const app = express()
-const PORT = process.env.PORT || 3000
-const pathPublic = path.resolve('public')
-const samplesPath = path.join(pathPublic, '../samples/')
-const renamePath = path.join('/workspaces/pdf-extract/', 'samples/')
+const app = express();
+const PORT = process.env.PORT || 3000;
+const pathPublic = path.resolve("public");
+const samplesPath = path.join(pathPublic, "../samples/");
+//const renamePath = path.join("/workspaces/pdf-extract/", "samples/")
+const renamePath = path.join(
+  "C:/Users/Utilisateur/Documents/Développement/CGSS/pdf-extract/",
+  "samples/"
+);
 
-app.use('/', express.static(pathPublic))
-app.use(fileUpload())
+app.use("/", express.static(pathPublic));
+app.use(fileUpload());
 
-app.post('/upload', (req, res) => {
-  let pdfFile
-  let uploadPath
-  let newNameFile
+app.post("/upload", (req, res) => {
+  let pdfFile;
+  let uploadPath;
+  let newNameFile;
+  let dateSynchronisation;
 
-  getCurrentFilenames()
+  getCurrentFilenames();
 
   if (!req.files && !req.files.pdfFile) {
-    return res.status(400).json({ message: 'No file uploaded' })
+    return res.status(400).json({ message: "No file uploaded" });
   }
-  console.log('req.files.pdfFile', req.files.pdfFile)
+  dateSynchronisation = String(req.body.dateJournalFluxSynchronisation);
+  console.log("dateSynchronisation", dateSynchronisation);
+  pdfFile = req.files.pdfFile;
+  const oldNameFile = pdfFile.name;
+  uploadPath = path.join(samplesPath, pdfFile.name);
+  console.log("uploadPath", uploadPath);
 
-  pdfFile = req.files.pdfFile
+  /* if (!fs.existsSync(samplesPath)) {
+    fs.mkdirSync(samplesPath);
+  }*/
+  fs.writeFileSync(uploadPath, pdfFile.data);
 
-  uploadPath = samplesPath + pdfFile.name
-  console.log('uploadPath', uploadPath)
-
-  if (!fs.existsSync(samplesPath)) {
-    fs.mkdirSync(samplesPath)
-  }
-  fs.writeFileSync(uploadPath, pdfFile.data)
-
-  const pdfExtract = new PDFExtract()
-  const options = {}
+  const pdfExtract = new PDFExtract();
+  const options = {};
 
   //const buffer = fs.readFileSync(uploadPath)
 
   pdfExtract.extract(String(uploadPath), options, (err, data) => {
     if (err) {
-      console.log(err)
-      return res.status(500).json({ message: 'Error extracting PDF' })
+      console.log(err);
+      return res.status(500).json({ message: "Error extracting PDF" });
     } else {
-      /* console.log(
-        'data.pages[0].pageInfo extractData.js',
-        data.pages[0].pageInfo,
-      ) */
-      /* console.log('data.pages[0].content extractData.js', data.pages[0].content) */
-
       data.pages[0].content.map((element) => {
         if (
           element.str
             .toLowerCase()
             .includes(
-              'modification agent dans le flux de synchronisation fichier: ',
+              "modification agent dans le flux de synchronisation fichier: "
             )
         ) {
-          //console.log('element.str', element.str)
-          newNameFile = element.str.split(': ')[1]
-          fs.rename(
-            String(uploadPath),
-            `${renamePath}${newNameFile}.pdf`,
-            (err) => {
-              if (err) {
-                console.log(err)
-                return res.status(500).json({ message: 'Error renaming file' })
-              } else {
-                console.log('File renamed successfully')
-                return res
-                  .status(200)
-                  .json({ message: 'File uploaded and renamed successfully' })
-              }
-            },
-          )
-        }
-      })
-      /* console.log('data.pages[0].content extractData.js', data.pages[0].content.splice(12, data.pages[0].content.length - 11)) */
+          newNameFile = element.str.split(": ")[1];
 
-      let dataTable = createJsonFromTable(
-        data.pages[0].content.splice(12, data.pages[0].content.length - 11),
-        newNameFile,
-      )
-      //console.log("dataTable",dataTable)
+          const filePath = `${renamePath}${newNameFile}.pdf`;
+          if (fs.existsSync(filePath)) {
+            const erasePath = `${renamePath}/${oldNameFile}`;
+            console.log("erasePath", erasePath);
+            fs.unlink(String(erasePath), (err) => {
+              if (err) {
+                console.log(err);
+                return res
+                  .status(500)
+                  .json({ message: "Erreur pour effacer le old file" });
+              }
+              console.log("fichier a été effacé.");
+            });
+            return res
+              .status(400)
+              .json({ message: "Le fichier existe déjà.", data: {} });
+          } else {
+            let dataTable = createJsonFromTable(
+              data.pages[0].content.splice(
+                12,
+                data.pages[0].content.length - 12
+              ),
+              newNameFile,
+              dateSynchronisation
+            );
+            const modifiedObj = removeLastTwoElementsFromObjectArray(
+              dataTable,
+              newNameFile
+            );
+
+            fs.rename(
+              String(uploadPath),
+              `${renamePath}${newNameFile}.pdf`,
+              (err) => {
+                if (err) {
+                  console.log(err);
+                  return res
+                    .status(500)
+                    .json({ message: "Erreur de renoomage du fichier" });
+                } else {
+                  console.log("Fichier renommé avec succès");
+                }
+              }
+            );
+            return res.status(200).json({
+              message: "Fichier téléchargé et renommé avec succès",
+              data: modifiedObj,
+            });
+          }
+        }
+      });
     }
-  })
-})
+  });
+});
 
 function getCurrentFilenames() {
-  console.log('Current filenames:')
+  console.log("Current filenames:");
   fs.readdirSync(samplesPath).forEach((file) => {
-    console.log(file)
-  })
+    console.log(file);
+  });
 }
 
-function createJsonFromTable(data, name) {
-  const result = {}
-  const result_agents = {
-    agent: '',
-    entite: '',
-    action: '',
-    modification: '',
-    avant: '',
-    apres: '',
-  }
-  const agentsArray = []
-  //console.log("data",data)
+function createJsonFromTable(data, name, dateSynchronisation) {
+  const result = {};
+  const agentsMap = new Map();
 
-  data.forEach((element, index) => {
-    /* if (index >= 0 && index <= 5) {
-      console.log('element', element, Math.round(element.x))
-    } */
+  let currentAgent = null;
 
-    if (Math.round(element.x) === 40) {
-      if (result_agents.agent !== '') {
-        agentsArray.push({ ...result_agents })
-        result_agents.agent = ''
-        result_agents.entite = ''
-        result_agents.action = ''
-        result_agents.modification = ''
-        result_agents.avant = ''
-        result_agents.apres = ''
+  data.forEach((element) => {
+    const x = Math.round(element.x);
+
+    if (x === 40) {
+      currentAgent = element.str;
+
+      if (!agentsMap.has(currentAgent)) {
+        agentsMap.set(currentAgent, {
+          numAgent: extractAgentNumber(currentAgent),
+          agent: currentAgent,
+          entite: "",
+          dateSynchronisation: dateSynchronisation,
+          lignes: [],
+        });
       }
-      result_agents.agent = element.str
-    } else if (Math.round(element.x) === 265) {
-      result_agents.entite += element.str + ' '
-    } else if (Math.round(element.x) === 431 || Math.round(element.x) === 432) {
-      result_agents.action = element.str
-    } else if (Math.round(element.x) === 455) {
-      result_agents.modification = element.str
-    } else if (Math.round(element.x) === 605) {
-      result_agents.avant = element.str
-    } else if (Math.round(element.x) === 705) {
-      result_agents.apres = element.str
+    } else if (x === 265) {
+      if (currentAgent) {
+        agentsMap.get(currentAgent).entite += element.str + " ";
+      }
+    } else if (x === 431 || x === 432) {
+      if (currentAgent) {
+        agentsMap.get(currentAgent).lignes.push({
+          action: element.str,
+          modification: "",
+          avant: "",
+          apres: "",
+        });
+      }
+    } else if (x === 455) {
+      if (currentAgent) {
+        const lignes = agentsMap.get(currentAgent).lignes;
+        lignes[lignes.length - 1].modification = element.str;
+      }
+    } else if (x === 605) {
+      if (currentAgent) {
+        const lignes = agentsMap.get(currentAgent).lignes;
+        lignes[lignes.length - 1].avant = element.str;
+      }
+    } else if (x === 705) {
+      if (currentAgent) {
+        const lignes = agentsMap.get(currentAgent).lignes;
+        lignes[lignes.length - 1].apres = element.str;
+      }
     }
-  })
+  });
 
-  if (result_agents.agent !== '') {
-    agentsArray.push({ ...result_agents });
-  }
-
-  result[name] = agentsArray
-  return result
+  result[name] = Array.from(agentsMap.values());
+  return result;
 }
 
-app.listen(PORT)
+function removeLastTwoElementsFromObjectArray(obj, key) {
+  if (obj[key] && Array.isArray(obj[key])) {
+    // Remove the last two elements from the array
+    obj[key].splice(-2, 2);
+  }
+  return obj;
+}
+
+function extractAgentNumber(agentString) {
+  const regex = /\((\d+)\)/;
+  const match = agentString.match(regex);
+
+  if (match) {
+    return Number(match[1]);
+  } else {
+    return null;
+  }
+}
+
+app.listen(PORT, () =>
+  console.log(`server running on http://localhost:${PORT}`)
+);
